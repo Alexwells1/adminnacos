11d652e43a0fd53157f8e328f1316b8eab80d5ae
Alexwells1 <egbetolaayomikun@gmail.com>
2025-10-21 06:31:08 +0100

    axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 // src/lib/axios.ts
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://colcom-backend.onrender.com/api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
